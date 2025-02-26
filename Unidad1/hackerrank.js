afb431704e0f614b6a7106311fab8fd0d82e2262
IramPortilla <daniel_igp@comunidad.unam.mx>
2025-02-25 21:33:34 -0600

    CSS

---
+++ Unidad1/hackerrank.js
@@ -0,0 +1,195 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/**
+ *   A line of code that prints "Hello, World!" on a new line is provided in the editor.
+ *   Write a second line of code that prints the contents of 'parameterVariable' on a new line.
+ *
+ *	Parameter:
+ *   parameterVariable - A string of text.
+ **/
+function greeting(parameterVariable) {
+    // This line prints 'Hello, World!' to the console:
+    console.log('Hello, World!');
+    console.log(parameterVariable)
+    // Write a line of code that prints parameterVariable to stdout using console.log:
+
+}
+
+
+function main() {
+    const parameterVariable = readLine();
+
+    greeting(parameterVariable);
+}
+
+
+
+
+
+
+/**
+ *   The variables 'firstInteger', 'firstDecimal', and 'firstString' are declared for you -- do not modify them.
+ *   Print three lines:
+ *   1. The sum of 'firstInteger' and the Number representation of 'secondInteger'.
+ *   2. The sum of 'firstDecimal' and the Number representation of 'secondDecimal'.
+ *   3. The concatenation of 'firstString' and 'secondString' ('firstString' must be first).
+ *
+ *	Parameter(s):
+ *   secondInteger - The string representation of an integer.
+ *   secondDecimal - The string representation of a floating-point number.
+ *   secondString - A string consisting of one or more space-separated words.
+ **/
+function performOperation(secondInteger, secondDecimal, secondString) {
+    // Declare a variable named 'firstInteger' and initialize with integer value 4.
+    const firstInteger = 4;
+
+    // Declare a variable named 'firstDecimal' and initialize with floating-point value 4.0.
+    const firstDecimal = 4.0;
+
+    // Declare a variable named 'firstString' and initialize with the string "HackerRank".
+    const firstString = 'HackerRank ';
+
+    // Write code that uses console.log to print the sum of the 'firstInteger' and 'secondInteger' (converted to a Number        type) on a new line.
+    console.log(firstInteger+Number(secondInteger))
+
+    // Write code that uses console.log to print the sum of 'firstDecimal' and 'secondDecimal' (converted to a Number            type) on a new line.
+    console.log(firstDecimal+Number(secondDecimal))
+
+    // Write code that uses console.log to print the concatenation of 'firstString' and 'secondString' on a new line. The        variable 'firstString' must be printed first.
+
+    console.log(firstString+secondString)
+}
+
+
+function main() {
+    const secondInteger = readLine();
+    const secondDecimal = readLine();
+    const secondString = readLine();
+
+    performOperation(secondInteger, secondDecimal, secondString);
+}
+
+
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/**
+ *   Calculate the area of a rectangle.
+ *
+ *   length: The length of the rectangle.
+ *   width: The width of the rectangle.
+ *
+ *	Return a number denoting the rectangle's area.
+ **/
+function getArea(length, width) {
+    let area;
+    // Write your code here
+    area=length*width;
+    return area;
+}
+
+/**
+ *   Calculate the perimeter of a rectangle.
+ *
+ *	length: The length of the rectangle.
+ *   width: The width of the rectangle.
+ *
+ *	Return a number denoting the perimeter of a rectangle.
+ **/
+function getPerimeter(length, width) {
+    let perimeter;
+    // Write your code here
+    perimeter=(2*length)+(2*width)
+    return perimeter;
+}
+
+
+function main() {
+    const length = +(readLine());
+    const width = +(readLine());
+
+    console.log(getArea(length, width));
+    console.log(getPerimeter(length, width));
+}
+
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+/*
+ * Create the function factorial here
+ */
+function factorial(n){
+    if(n==0){
+        return 1;
+    }
+    return n * factorial(n-1)
+}
+
+function main() {
+    const n = +(readLine());
+
+    console.log(factorial(n));
+}
