568da17beb789c4d17307de16350838b5a1a8c9a
@sebas0123hd <martin_sebastian@comunidad.unam.mx>
2025-02-25 21:30:51 -0600

    Primer ejercicio_1_js.js

---
+++ Unidad1/ejercicio_1_js.js
@@ -0,0 +1,121 @@
+/**
+ *   A line of code that prints "Hello, World!" on a new line is provided in the editor.
+ *   Write a second line of code that prints the contents of 'parameterVariable' on a new line.
+ *
+ *	Parameter:
+ *   parameterVariable - A string of text.
+ **/
+function greeting(parameterVariable) {
+    // This line prints 'Hello, World!' to the console:
+    console.log('Hello, World!');
+    console.log(parameterVariable);
+    // Write a line of code that prints parameterVariable to stdout using console.log:
+
+}
+
+
+
+
+/**
+ *   The variables 'firstInteger', 'firstDecimal', and 'firstString' are declared for you -- do not modify them.
+ *   Print three lines:
+ *   1. The sum of 'firstInteger' and the Number representation of 'secondInteger'.
+ *   2. The sum of 'firstDecimal' and the Number representation of 'secondDecimal'.
+ *   3. The concatenation of 'firstString' and 'secondString' ('firstString' must be first).
+ *
+ *	Parameter(s):
+ *   secondInteger - The string representation of an integer.
+ *   secondDecimal - The string representation of a floating-point number.
+ *   secondString - A string consisting of one or more space-separated words.
+ **/
+function performOperation(secondInteger, secondDecimal, secondString) {
+    // Declare a variable named 'firstInteger' and initialize with integer value 4.
+    const firstInteger = 4;
+
+    // Declare a variable named 'firstDecimal' and initialize with floating-point value 4.0.
+    const firstDecimal = 4.0;
+
+    // Declare a variable named 'firstString' and initialize with the string "HackerRank".
+    const firstString = 'HackerRank ';
+
+    // Write code that uses console.log to print the sum of the 'firstInteger' and 'secondInteger' (converted to a Number        type) on a new line.
+    console.log(firstInteger+Number(secondInteger))
+
+    // Write code that uses console.log to print the sum of 'firstDecimal' and 'secondDecimal' (converted to a Number            type) on a new line.
+    console.log(firstDecimal+Number(secondDecimal))
+
+    // Write code that uses console.log to print the concatenation of 'firstString' and 'secondString' on a new line. The        variable 'firstString' must be printed first.
+    console.log(firstString+String(secondString))
+}
+
+
+
+
+
+/**
+ *   Calculate the area of a rectangle.
+ *
+ *   length: The length of the rectangle.
+ *   width: The width of the rectangle.
+ *
+ *	Return a number denoting the rectangle's area.
+ **/
+function getArea(length, width) {
+    let area = length * width;
+    // Write your code here
+
+    return area;
+}
+
+/**
+ *   Calculate the perimeter of a rectangle.
+ *
+ *	length: The length of the rectangle.
+ *   width: The width of the rectangle.
+ *
+ *	Return a number denoting the perimeter of a rectangle.
+ **/
+function getPerimeter(length, width) {
+    let perimeter = (2*length)+(2 * width);
+    // Write your code here
+
+    return perimeter;
+}
+
+
+
+
+
+/*
+ * Create the function factorial here
+ */
+function factorial(n) {
+    let factorial = 1
+    for (let i = 1; i <= n; i++) {
+        factorial = factorial * i
+    }
+    return factorial;
+}
+
+
+
+
+
+
+/*
+ * Complete the vowelsAndConsonants function.
+ * Print your output using 'console.log()'.
+ */
+function vowelsAndConsonants(s) {
+    const vocales = ["a", "e", "i", "o", "u"]
+    for (let i = 0; i <= s.length; i++) {
+        if (vocales.includes(s.charAt(i)))
+
+            console.log(s.charAt(i))
+    }
+    for (let i = 0; i <= s.length; i++) {
+        if (!vocales.includes(s.charAt(i)))
+
+            console.log(s.charAt(i))
+    }
+}
