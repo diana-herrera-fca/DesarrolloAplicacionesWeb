e7d530c33423db7406f67de9e88ee325d6d4a28e
rafikiifca <112977045+rafikiifca@users.noreply.github.com>
2025-02-27 19:31:51 -0600

    Practica3.js

--- Unidad1/Practica3.js
+++ Unidad1/Practica3.js
@@ -177,3 +177,540 @@ function main() {
 }
 
 //DIA 1 EJERCICIO 2
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+/*
+ * Create the function factorial here
+ */
+
+function factorial(n) {
+    let result = 1;
+    for (let i = 2; i <= n; i++) {
+        result *= i;
+    }
+    return result;
+}
+
+
+function main() {
+    const n = +(readLine());
+    
+    console.log(factorial(n));
+}
+
+//DIA 1 EJERCICIO 3
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+function main() {
+    const PI = Math.PI;
+    let r = parseFloat(readLine());
+    // Print the area of the circle:
+    let area = PI * Math.pow(r,2);
+    console.log(area);
+    
+    // Print the perimeter of the circle:
+    var perimeter = 2 * PI * r;
+    console.log(perimeter);
+
+    try {    
+        // Attempt to redefine the value of constant variable PI
+        PI = 0;
+        // Attempt to print the value of PI
+        console.log(PI);
+    } catch(error) {
+        console.error("You correctly declared 'PI' as a constant.");
+    }
+}
+
+//DIA 2 EJERCICIO 1
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+function getGrade(score) {
+    let grade;
+    // Write your code here
+        if (score > 25 && score <= 30) {
+        grade = 'A';
+    } else if (score > 20 && score <= 25) {
+        grade = 'B';
+    } else if (score > 15 && score <= 20) {
+        grade = 'C';
+    } else if (score > 10 && score <= 15) {
+        grade = 'D';
+    } else if (score > 5 && score <= 10) {
+        grade = 'E';
+    } else {
+        grade = 'F';
+    }
+    
+    return grade;
+}
+
+function main() {
+    const score = +(readLine());
+    
+    console.log(getGrade(score));
+}
+
+//DIA 2 EJERCICIO 2
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+function getLetter(s) {
+    let letter;
+    // Write your code here
+    switch (s.charAt(0)) {
+        case 'a': case 'e': case 'i': case 'o': case 'u':
+            letter = 'A';
+            break;
+                
+        case 'b': case 'c': case 'd': case 'f': case 'g':
+            letter = 'B';
+            break;
+                
+        case 'h': case 'j': case 'k': case 'l': case 'm':
+            letter = 'C';
+            break;
+                
+        default:
+            letter = 'D';
+            break;
+    }
+
+    return letter;
+}
+
+
+function main() {
+    const s = readLine();
+    
+    console.log(getLetter(s));
+}
+
+//DIA 2 EJERCICIO 3
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the vowelsAndConsonants function.
+ * Print your output using 'console.log()'.
+ */
+function vowelsAndConsonants(s) {
+    const vowels = ['a', 'e', 'i', 'o', 'u'];
+    let consonants = [];
+
+    for (let i = 0; i < s.length; i++) {
+        if (vowels.includes(s[i])) {
+            console.log(s[i]);
+        } else {
+            consonants.push(s[i]);
+        }
+    }
+
+    for (let i = 0; i < consonants.length; i++) {
+        console.log(consonants[i]);
+    }
+}
+
+
+
+function main() {
+    const s = readLine();
+    
+    vowelsAndConsonants(s);
+}
+
+//DIA 3 EJERCICIO 1
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/**
+*   Return the second largest number in the array.
+*   @param {Number[]} nums - An array of numbers.
+*   @return {Number} The second largest number in the array.
+**/
+function getSecondLargest(nums) {
+    // Complete the function
+const sorted_array          = nums.sort((a,b) => a - b );
+    const unique_sorted_array   = [...new Set(sorted_array)];
+    return unique_sorted_array[unique_sorted_array.length - 2];
+}
+
+
+function main() {
+    const n = +(readLine());
+    const nums = readLine().split(' ').map(Number);
+    
+    console.log(getSecondLargest(nums));
+}
+
+//DIA 3 EJERCICIO 2
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the reverseString function
+ * Use console.log() to print to stdout.
+ */
+function reverseString(s) {
+    try {
+        console.log(s.split("").reverse().join(""));
+    } catch (error) {
+        console.log(error.message);
+        console.log(s);
+    }
+}
+
+
+function main() {
+    const s = eval(readLine());
+    
+    reverseString(s);
+}
+
+//DIA 3 EJERCICIO 3
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the isPositive function.
+ * If 'a' is positive, return "YES".
+ * If 'a' is 0, throw an Error with the message "Zero Error"
+ * If 'a' is negative, throw an Error with the message "Negative Error"
+ */
+function isPositive(a) {
+     if (a > 0)
+        {
+            return "YES";
+        }
+    else if (a === 0)
+        {
+            throw new Error("Zero Error");
+        }
+    else if (a < 0)
+        {
+            throw new Error("Negative Error");
+        }
+}
+
+
+function main() {
+    const n = +(readLine());
+    
+    for (let i = 0; i < n; i++) {
+        const a = +(readLine());
+      
+        try {
+            console.log(isPositive(a));
+        } catch (e) {
+            console.log(e.message);
+        }
+    }
+}
+
+//DIA 4 EJERCICIO 1
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the Rectangle function
+ */
+function Rectangle(a, b) {
+    this.length = a;
+    this.width = b;
+    this.perimeter = 2 * (a+b);
+    this.area = a * b;
+}
+
+
+function main() {
+    const a = +(readLine());
+    const b = +(readLine());
+    
+    const rec = new Rectangle(a, b);
+    
+    console.log(rec.length);
+    console.log(rec.width);
+    console.log(rec.perimeter);
+    console.log(rec.area);
+}
+
+//DIA 4 EJERCICIO 2
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Return a count of the total number of objects 'o' satisfying o.x == o.y.
+ * 
+ * Parameter(s):
+ * objects: an array of objects with integer properties 'x' and 'y'
+ */
+function getCount(objects) {
+    var count = 0;
+    for (var index in objects)
+        {
+            if (objects[index].x == objects[index].y)
+                {
+                    count++;
+                }
+        }
+    return count;
+}
+
+
+function main() {
+    const n = +(readLine());
+    let objects = [];
+    
+    for (let i = 0; i < n; i++) {
+        const [a, b] = readLine().split(' ');
+        
+        objects.push({x: +(a), y: +(b)});
+    }
+    
+    console.log(getCount(objects));
+}
+
+//DIA 4 EJERCICIO 3
+/*
+ * Implement a Polygon class with the following properties:
+ * 1. A constructor that takes an array of integer side lengths.
+ * 2. A 'perimeter' method that returns the sum of the Polygon's side lengths.
+ */
+class Polygon {
+    constructor(heights) 
+    {
+        var polygon_perimeter = 0;
+        for (var index in heights)
+            {
+                polygon_perimeter += heights[index];
+            }
+        
+        this.my_perimeter = polygon_perimeter;
+    }
+    
+     perimeter() 
+    {
+        return this.my_perimeter;
+    }
+    
+}
+
+const rectangle = new Polygon([10, 20, 10, 20]);
+const square = new Polygon([10, 10, 10, 10]);
+const pentagon = new Polygon([10, 20, 30, 40, 43]);
+
+console.log(rectangle.perimeter());
+console.log(square.perimeter());
+console.log(pentagon.perimeter());
